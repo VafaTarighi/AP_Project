e98f80a7c81edfc970fe689f4f7c535b53c1ead0
Vafa <55409304+VafaTarighi@users.noreply.github.com>
2020-05-10 18:21:39 +0430

    Add files via upload

---
+++ AP_Project/src/main/java/bignumbers/BigNumber.java
@@ -0,0 +1,55 @@
+package bignumbers;
+
+import java.math.BigInteger;
+
+public class BigNumber implements Comparable<BigNumber> {
+
+    private int sign;
+
+    private byte[] digits;
+
+    public BigNumber(String val) {
+
+        sign = 1;
+        if (val.charAt(0) == '-') {
+            val = val.substring(1);
+            sign = -1;
+        }
+        else if (val.charAt(0) == '+') {
+            val = val.substring(1);
+        }
+
+        if (val.length() == 0)
+            throw new NumberFormatException("Zero length BigNumber");
+
+        if (!val.matches("\\d+")) {
+            StringBuilder message = new StringBuilder("For input string: \"");
+            int charIndex = 0;
+            for (int i = 0; i < val.length(); i++) {
+                if (!("" + val.charAt(i)).matches("\\d")) {
+                    charIndex = i;
+                    break;
+                }
+            }
+
+            String prb = val.substring(Math.max(charIndex - 4, 0),
+                    Math.min(charIndex + 4, val.length()));
+
+            message.append(prb).append("\"");
+
+            throw new NumberFormatException(message.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        BigInteger bi = new BigInteger("-1234567891011121314151617181920212223");
+        BigNumber bn = new BigNumber("-1123314a1235135134");
+
+    }
+
+    @Override
+    public int compareTo(BigNumber o) {
+        return 0;
+    }
+}
